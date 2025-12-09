f9302726de646ccd5135dc1ec3a72348013158c0
madesears <solankeadi11@gmail.com>
2025-12-10 02:30:15 +0530

    completed Three Dot Menu

--- app.js
+++ app.js
@@ -27,3 +27,34 @@ document.addEventListener('click', (event) => {
     }
 });
 
+
+const dotmenu = document.getElementById('dot-menu');
+const dot3btn = document.getElementById('dot3-btn');
+
+function threedotmenu(){
+    const isHidden = dotmenu.classList.contains('hidden');
+
+    if(isHidden){
+        dotmenu.classList.remove('hidden');
+        setTimeout( () => {
+            dotmenu.classList.add('opa-100', 'scale-100');
+            dotmenu.classList.remove('opa-0', 'scale-95');
+        }, 10);
+    }  else {
+        dotmenu.classList.remove('opa-100', 'scale-100');
+        dotmenu.classList.add('opa-0', 'scale-95');
+        setTimeout(()=>{ dotmenu.classList.add('hidden'); }, 150);
+    }
+
+    document.addEventListener('click', (event)=> {
+        if(!dotmenu) return;
+
+        const inside = event.target.closest('#dot-menu');
+        const toggle = event.target.closest('#dot3-btn');
+        if(!inside && !toggle && !dotmenu.classList.contains('hidden')){
+            dotmenu.classList.remove('opa-100', 'scale-100');
+            dotmenu.classList.add('opa-0', 'scale-95');
+            setTimeout(()=>{ dotmenu.classList.add('hidden'); }, 150);
+        }
+    });
+}
